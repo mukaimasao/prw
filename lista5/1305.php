3f1155d21f8c798923183b673848b3d6d55e1bc4
Mário <mmm.masaomukai@gmail.com>
2023-03-28 06:06:01 -0300

    alteração de nome

--- lista5/1305.php
+++ lista5/1305.php
@@ -2,18 +2,16 @@
 while ($input = fgets(STDIN)) {
   $num = floatval(trim($input));
   $cutoff = floatval(trim(fgets(STDIN)));
-  
+
   $integer_part = intval($num);
   $decimal_part = $num - $integer_part;
   
   $cutoff_decimal_part = $cutoff - intval($cutoff);
-  
   if ($decimal_part > $cutoff_decimal_part) {
-    echo $integer_part + 1 . "\n";
+    echo $integer_part + 1 . PHP_EOL;
   } else {
-    echo $integer_part . "\n";
+    echo $integer_part . PHP_EOL;
   }
 }
-
 
 ?>
